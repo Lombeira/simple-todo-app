6dd825ffcb834ad6b2352227c50c5fb111344a90
Glauco Lomenha <glauco.lomenha@memed.com.br>
2025-02-06 15:29:57 -0300

    test: added e2e use cases for todo app

--- src/mocks/user.js
+++ src/mocks/user.js
@@ -1,16 +1,15 @@
-import { v4 as uuidv4 } from "uuid";
 
 export const USERS_MOCK = [
   {
-    id: uuidv4(),
+    id: "1",
     name: "John Doe",
   },
   {
-    id: uuidv4(),
+    id: "2",
     name: "Jane Doe",
   },
   {
-    id: uuidv4(),
+    id: "3",
     name: "Glauco Doe",
   },
 ]
